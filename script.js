a20bb5a95f22613c1d7d9cb67f1c3ab0debda7d3
rohitprojects12 <147717777+rohitprojects12@users.noreply.github.com>
2023-10-29 18:45:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  // Redirect to signup website
+  window.location.href = "https://rohitjprojects.github.io/GYM-WEB/";
+});
+
+signInButton.addEventListener("click", () => {
+  // Redirect to login website
+  window.location.href = "https://rohitjprojects.github.io/GYM-WEB/";
+});
